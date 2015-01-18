3ae8929a16aeb670d328b3ca5ca6183db0d6817f
Armin.Schoenlieb <Armin.Schoenlieb.etm11@fh-joanneum.at>
2015-01-18 23:51:08 +0100

    Changed the makefile created first Led files changed main module to RFM12.c

---
+++ Modules/RFM12.c
@@ -0,0 +1,26 @@
+#include <LED.h>
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_AUTHOR("Remonda");
+MODULE_DESCRIPTION("A Simple Hello World module");
+MODULE_LICENSE("GPL");
+
+static int __init hello_init(void)
+{
+	pr_debug("Hello world!\n");
+//	init_Led();
+	/* Non-zero return means that the module couldn't be loaded. */
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_debug("Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
