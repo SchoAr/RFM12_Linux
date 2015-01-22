6b3dd2dc65cd8929760d23369254cfc18b0bf063
Armin.Schoenlieb <Armin.Schoenlieb.etm11@fh-joanneum.at>
2015-01-22 21:37:14 +0100

    added test for the gpio

--- Modules/RFM12.c
+++ Modules/RFM12.c
@@ -1,23 +1,45 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/gpio.h>
 
 MODULE_AUTHOR("Schoenlieb");
 MODULE_DESCRIPTION("A Simple Hello World module");
 MODULE_LICENSE("GPL");
 
+
+static struct gpio leds[] = {
+    { 15, GPIOF_OUT_INIT_HIGH, "USERLED" },
+};
+
 static int __init hello_init(void)
 {
-	pr_debug("Hello world!\n");
-	/* Non-zero return means that the module couldn't be loaded. */
-	return 0;
+    int ret = 0;
+    int i; 
+    
+    ret = gpio_request_array(leds, ARRAY_SIZE(leds));
+    
+    if(ret){
+	printk(KERN_ERR "Unable to request gpio for LEDs  %d",ret);
+	return ret;
+    }
+    
+    for(i = 0; i < ARRAY_SIZE(leds); i++){
+	gpio_set_value(leds[i].gpio,1);
+    }
+      
+    return ret;
 }
-
 static void __exit hello_exit(void)
 {
-    pr_debug("Cleaning up module.\n");
+    int i; 
+    
+    for(i = 0; i < ARRAY_SIZE(leds); i++){
+	gpio_set_value(leds[i].gpio,0);
+    }
+    
+    gpio_free_array(leds,ARRAY_SIZE(leds));
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
-
