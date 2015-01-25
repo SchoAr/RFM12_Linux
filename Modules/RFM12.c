72f29c56b6cced5989837308a31f846f2920d270
Armin.Schoenlieb <Armin.Schoenlieb.etm11@fh-joanneum.at>
2015-01-25 20:19:36 +0100

    added the GPIO Interrupt Made a demo with the Button

--- Modules/RFM12.c
+++ Modules/RFM12.c
@@ -2,35 +2,101 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/gpio.h>
+#include <linux/interrupt.h> 
 #include <linux/spi/spi.h>
 #include "RFM12_config.h"
 
 MODULE_AUTHOR("Schoenlieb");
 MODULE_DESCRIPTION("A Char Driver for the RFM12 Radio Transceiver");
 MODULE_LICENSE("GPL");
+/********************/
+int ledstatus = 0;
+/********************/
 
+/*
+ * Define for The LEDs
+ */
 static struct gpio leds[] = {
     { ON_LED, GPIOF_OUT_INIT_HIGH, "ON" },
     { RX_LED, GPIOF_OUT_INIT_HIGH, "RX" },
     { TX_LED, GPIOF_OUT_INIT_HIGH, "TX" },
 };
 
+/*
+ * Define for the Inputs	 
+ */ 
+static struct gpio input[] = {
+    { INPUTPIN, GPIOF_IN, "INPUT" },
+};
+/*
+ *  Defines for the Interrupt 
+ */
+static int input_irqs[] = { -1 };
+
+
+static irqreturn_t input_ISR (int irq, void *data)
+{
+#ifdef DEBUG
+  printk(KERN_INFO"Intterupt Occured.\n");
+#endif
+  
+  if(irq == input_irqs[0]) {
+      if(ledstatus){
+	gpio_set_value(leds[0].gpio,0);
+	ledstatus = 0;
+      }
+      else{
+	gpio_set_value(leds[0].gpio,1);
+	ledstatus = 1;
+      }	  
+    }
+    return IRQ_HANDLED;
+}
+
 static int __init RFM_init(void)
 {
     int ret = 0;
     int i; 
-    
+    /*Request LEDs*/
     ret = gpio_request_array(leds, ARRAY_SIZE(leds));
-    
     if(ret){
-	printk(KERN_ERR "Unable to request gpio for LEDs  %d",ret);
-	return ret;
+      printk(KERN_ERR "Unable to request gpio for LEDs  %d",ret);
+      return ret;
+    }
+    /*Turn LEDs on */
+    for(i = 0; i < ARRAY_SIZE(leds); i++){
+    	gpio_set_value(leds[i].gpio,1);
+    }
+    ledstatus = 1;
+    /*Request IRQ*/
+    ret = gpio_request_array(input, ARRAY_SIZE(input));
+    
+    if (ret) {
+      printk(KERN_ERR "Unable to request GPIOs for BUTTONs: %d\n", ret);
+      goto fail1;
     }
     
-    for(i = 0; i < ARRAY_SIZE(leds); i++){
-	gpio_set_value(leds[i].gpio,1);
+    ret = gpio_to_irq(input[0].gpio);
+    if(ret < 0) {
+      printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
+      goto fail2;
     }
-      
+    
+    input_irqs[0] = ret;
+    printk(KERN_INFO "Successfully requested INPUT1 IRQ # %d\n", input_irqs[0]);
+    
+    ret = request_irq(input_irqs[0], input_ISR, IRQF_TRIGGER_RISING | IRQF_DISABLED, "gpiomod#input1", NULL);
+    if(ret) {
+      printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
+      goto fail2;
+    }
+    return 0;
+    
+    fail2:
+      gpio_free_array(input, ARRAY_SIZE(input));
+    fail1:
+      gpio_free_array(leds, ARRAY_SIZE(leds));
+    
     return ret;
 }
 
@@ -40,9 +106,12 @@ static void __exit RFM_exit(void)
     
     for(i = 0; i < ARRAY_SIZE(leds); i++){
 	gpio_set_value(leds[i].gpio,0);
-    }
-    
+    } 
+    // free irqs
+    free_irq(input_irqs[0], NULL);
+    /* unregister */
     gpio_free_array(leds,ARRAY_SIZE(leds));
+    gpio_free_array(input, ARRAY_SIZE(input));
 }
 
 module_init(RFM_init);
