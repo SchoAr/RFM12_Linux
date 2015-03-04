84dafe88496a5fa318443d4feb200a4f41069abd
Armin.Schoenlieb <Armin.Schoenlieb.etm11@fh-joanneum.at>
2015-03-04 20:13:19 +0100

    put the SPI Init into a separate function to get a better overview

--- Modules/RFM12.c
+++ Modules/RFM12.c
@@ -94,9 +94,7 @@ static int init_Gpio(void){
       printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
       goto fail2;
     }
-/*
- **************DEBUG ledstatus 
- */
+ /***************DEBUG ledstatus  */
     ledstatus = 1;
 /*Only when On LED is avaiable it can be set to indicate the driver is working*/ 
 #ifdef ON_LED_ENABLE
@@ -111,6 +109,139 @@ static int init_Gpio(void){
     
     return ret;
 }
+/***************SPI Operations************************/
+#define SPI_BUS 32766
+#define SPI_BUS_CS0 0	
+#define SPI_BUS_SPEED 200000
+
+struct spi_master *spi_master;
+struct spi_device *spi_device;
+struct spi_message msg;
+
+static int RFM12_probe(struct spi_device *spi_devicef)
+{
+    spi_device = spi_devicef;
+    return 0;
+}
+ static int RFM12_remove(struct spi_device *spi_devicef)
+{
+    spi_device = NULL;
+    return 0;
+}
+
+static struct spi_driver RFM12_driver = {
+    .driver = {
+      .name = "RFM12_spi",
+      .owner = THIS_MODULE,
+      },
+      .probe = RFM12_probe,
+      .remove = RFM12_remove,
+};
+
+char buff[64];
+struct device *pdev;
+char tx_buff[26];
+struct spi_transfer transfer = {
+        .tx_buf         = tx_buff,
+	.rx_buf 	= 0,
+        .len            = 4,
+    };
+
+static int init_Spi(void){
+  
+    int status; 
+    int i; 
+    status = spi_register_driver(&RFM12_driver);
+    
+    printk(KERN_INFO "Register driver Successfully Initialized !\n");
+    
+    if (status < 0) {
+	printk(KERN_ALERT "spi_register_driver() failed %d\n", status);
+	return status;
+    }
+    
+    spi_master = spi_busnum_to_master(SPI_BUS);
+    if (!spi_master) {
+      printk(KERN_ALERT "spi_busnum_to_master(%d) returned NULL\n",SPI_BUS);
+      return -1;
+    }
+    printk(KERN_INFO "spi busnum  Successfully Initialized !\n");
+    spi_device = spi_alloc_device(spi_master);
+    if (!spi_device) {
+      put_device(&spi_master->dev);
+      printk(KERN_ALERT "spi_alloc_device() failed\n");
+      return -1;
+    }
+    
+    spi_device->chip_select = SPI_BUS_CS0; 
+    printk(KERN_INFO "SPI CS set !\n");
+    
+    /* Check whether this SPI bus.cs is already claimed */
+     snprintf(buff, sizeof(buff), "%s.%u",
+	      dev_name(&spi_device->master->dev),
+	      spi_device->chip_select);
+      
+     pdev = bus_find_device_by_name(spi_device->dev.bus, NULL, buff);
+     printk(KERN_INFO "Spi bus find device by name  !\n");
+     if (pdev) {
+	    /* We are not going to use this spi_device, so free it */
+	  printk(KERN_INFO "SPI Device already exists !\n");
+	    spi_dev_put(spi_device);
+	  /*
+	  * There is already a device configured for this bus.cs
+	  * It is okay if it us, otherwise complain and fail.
+	  */
+	  if (pdev->driver && pdev->driver->name &&
+	  strcmp("RFM12_spi", pdev->driver->name)) {
+	      printk(KERN_ALERT
+	      "Driver [%s] already registered for %s\n",
+	      pdev->driver->name, buff);
+	      status = -1;
+	  } 
+	printk(KERN_INFO "Returning !\n");  
+	return status;
+     }   
+    printk(KERN_INFO "spi driver checked if afauable !\n");
+    
+    spi_device->max_speed_hz = SPI_BUS_SPEED;
+    spi_device->mode = SPI_MODE_0;
+    spi_device->bits_per_word = 8;
+    spi_device->irq = -1;
+    spi_device->controller_state = NULL;
+    spi_device->controller_data = NULL;
+    
+    strlcpy(spi_device->modalias, "RFM12_SPI", SPI_NAME_SIZE);
+    
+    printk(KERN_INFO "SPI READY FOR SEND !\n");
+    
+    status = spi_add_device(spi_device);
+    if (status < 0) {
+	spi_dev_put(spi_device);
+	printk(KERN_ALERT "spi_add_device() failed: %d\n",status);
+    } 
+    printk(KERN_INFO "SPI DEVICE added !\n");
+    
+    tx_buff[2] = i++;
+    tx_buff[0] = i++;
+    tx_buff[1] = i++;
+    tx_buff[3] = i++;
+    
+    spi_message_init(&msg);
+    
+    spi_message_add_tail(&transfer,&msg); 
+    printk(KERN_INFO "SPI MESSAGE ADDED TAIL !\n");
+    status = spi_sync(spi_device, &msg);
+    
+    if(status){
+      printk(KERN_ALERT "SPI Transfer Failed: %d\n",status);
+      spi_unregister_device(spi_device);
+      spi_unregister_driver(&RFM12_driver);
+      return status;
+    }
+    printk(KERN_INFO "SPI Transfer Successfully !\n");   
+    return status;
+}
+    
 /***************File Operations************************/
 static const char *id = "Hello World";
 
@@ -145,142 +276,20 @@ static struct miscdevice eud_dev = {
 	.fops           = &fops
 };
 
-#define SPI_BUS 32766
-#define SPI_BUS_CS0 0	
-#define SPI_BUS_SPEED 50000
-
-    struct spi_master *spi_master;
-    struct spi_device *spi_device;
-    struct spi_message msg;
-
-int i;
-
-static int RFM12_probe(struct spi_device *spi_devicef)
-{
-    spi_device = spi_devicef;
-    return 0;
-}
- static int RFM12_remove(struct spi_device *spi_devicef)
-{
-    spi_device = NULL;
-    return 0;
-}
-
-    static struct spi_driver RFM12_driver = {
-    .driver = {
-      .name = "RFM12_spi",
-      .owner = THIS_MODULE,
-      },
-      .probe = RFM12_probe,
-      .remove = RFM12_remove,
-};
-
 static int __init RFM_init(void)
 {
     int status = 0;
-    char buff[64];
-     struct device *pdev;
-    char tx_buff[26];
-    struct spi_transfer transfer = {
-        .tx_buf         = tx_buff,
-	.rx_buf 	= 0,
-        .len            = 4,
-    };
-       
-    status = spi_register_driver(&RFM12_driver);
     
-    printk(KERN_INFO "Register driver Successfully Initialized !\n");
-    
-    if (status < 0) {
-	printk(KERN_ALERT "spi_register_driver() failed %d\n", status);
-	return status;
-    }
-    
-    spi_master = spi_busnum_to_master(SPI_BUS);
-    if (!spi_master) {
-      printk(KERN_ALERT "spi_busnum_to_master(%d) returned NULL\n",SPI_BUS);
-      return -1;
-    }
-    printk(KERN_INFO "spi busnum  Successfully Initialized !\n");
-    spi_device = spi_alloc_device(spi_master);
-    if (!spi_device) {
-      put_device(&spi_master->dev);
-      printk(KERN_ALERT "spi_alloc_device() failed\n");
-      return -1;
-    }
-    
-    spi_device->chip_select = SPI_BUS_CS0; 
-    printk(KERN_INFO "SPI CS set !\n");
-    
-    /* Check whether this SPI bus.cs is already claimed */
-     snprintf(buff, sizeof(buff), "%s.%u",
-	      dev_name(&spi_device->master->dev),
-	      spi_device->chip_select);
-      
-     pdev = bus_find_device_by_name(spi_device->dev.bus, NULL, buff);
-     printk(KERN_INFO "Spi bus find device by name  !\n");
-/*     if (pdev) {
-	    /* We are not going to use this spi_device, so free it */
-//	  printk(KERN_INFO "SPI Device already exists !\n");
-//	    spi_dev_put(spi_device);
-	  /*
-	  * There is already a device configured for this bus.cs
-	  * It is okay if it us, otherwise complain and fail.
-	  */
-/*	  if (pdev->driver && pdev->driver->name &&
-	  strcmp("RFM12_spi", pdev->driver->name)) {
-	      printk(KERN_ALERT
-	      "Driver [%s] already registered for %s\n",
-	      pdev->driver->name, buff);
-	      status = -1;
-	  } 
-	printk(KERN_INFO "Returning !\n");  
-	return status;
-     }*/   
-    printk(KERN_INFO "spi driver checked if afauable !\n");
-    
-    spi_device->max_speed_hz = SPI_BUS_SPEED;
-    spi_device->mode = SPI_MODE_0;
-    spi_device->bits_per_word = 8;
-    spi_device->irq = -1;
-    spi_device->controller_state = NULL;
-    spi_device->controller_data = NULL;
-    
-    strlcpy(spi_device->modalias, "RFM12_SPI", SPI_NAME_SIZE);
-    
-    printk(KERN_INFO "SPI READY FOR SEND !\n");
-    
-    status = spi_add_device(spi_device);
-    if (status < 0) {
-	spi_dev_put(spi_device);
-	printk(KERN_ALERT "spi_add_device() failed: %d\n",status);
-    } 
-    printk(KERN_INFO "SPI DEVICE added !\n");
-    
-    tx_buff[0] = i++;
-    tx_buff[1] = i++;
-    tx_buff[2] = i++;
-    tx_buff[3] = i++;
-    
-    spi_message_init(&msg);
-    
-    spi_message_add_tail(&transfer,&msg); 
-    printk(KERN_INFO "SPI MESSAGE ADDED TAIL !\n");
-    status = spi_sync(spi_device, &msg);
-    
-    if(status){
-      printk(KERN_ALERT "SPI Transfer Failed: %d\n",status);
-      spi_unregister_device(spi_device);
-      spi_unregister_driver(&RFM12_driver);
-      return status;
-    }
-    printk(KERN_INFO "SPI Transfer Successfully !\n");
-/**************************************************************/    
-
     /* Register GPIO and Interrupt)*/
     status = init_Gpio();
     if (status!= 0){
       return status; 
+    }
+    /*Reguster SPI Communication*/
+    status = init_Spi();
+    if(status != 0){
+      printk(KERN_ALERT "SPI initialization failed: %d\n",status);
+      return status;
     }
     /*Initialize the File Operations*/
     status = misc_register(&eud_dev);
