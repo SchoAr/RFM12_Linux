43c27af8997bceab5803c044150e185008f3eef1
Armin.Schoenlieb <Armin.Schoenlieb.etm11@fh-joanneum.at>
2015-09-15 20:17:00 +0200

    Delted spaces and not needed stuff

--- Modules/RFM12.c
+++ Modules/RFM12.c
@@ -65,10 +65,10 @@ static struct gpio leds[] = {
 #endif
 #ifdef TX_LED_ENABLE
     { TX_LED, GPIOF_OUT_INIT_HIGH, "TX" },
-#endif  
+#endif
 };
 
-/*Define for the Inputs*/ 
+/*Define for the Inputs*/
 static struct gpio input[] = {
     { IRQ_PIN, GPIOF_IN, "INPUT" },
 };
@@ -130,7 +130,7 @@ static void RFM12_work_handler(struct work_struct *w){
 		*/
 		xfer2(RF_TXREG_WRITE + out);
 	}
-        printk("Work Queue ended\n");	  
+        printk("Work Queue ended\n");
 }
 unsigned long flags;
 static irqreturn_t input_ISR (int irq, void *data)
@@ -144,7 +144,7 @@ static irqreturn_t input_ISR (int irq, void *data)
 }
 
 static int init_Gpio(void){
-    
+
     int ret = 0;
     int i; 
     /*Request LEDs*/
@@ -157,24 +157,24 @@ static int init_Gpio(void){
     for(i = 0; i < ARRAY_SIZE(leds); i++){
 	gpio_set_value(leds[i].gpio,0);
     }
-   
+
     /*Request IRQ*/
     ret = gpio_request_array(input, ARRAY_SIZE(input));
-    
+
     if (ret) {
       printk(KERN_ERR "Unable to request GPIOs for BUTTONs: %d\n", ret);
       goto fail1;
     }
-    
+
     ret = gpio_to_irq(input[0].gpio);
     if(ret < 0) {
       printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
       goto fail2;
     }
-    
+
     input_irqs[0] = ret;
     printk(KERN_INFO "Successfully requested INPUT1 IRQ # %d\n", input_irqs[0]);
-    
+
     ret = request_irq(input_irqs[0], input_ISR, IRQF_TRIGGER_FALLING | IRQF_DISABLED, "gpiomod#input1", NULL);
     if(ret) {
       printk(KERN_ERR "Unable to request IRQ: %d\n", ret);
@@ -188,17 +188,15 @@ static int init_Gpio(void){
     }
 /***************DEBUG ledstatus  */
     ledstatus = 1;
-/*Only when On LED is avaiable it can be set to indicate the driver is working*/ 
+/*Only when On LED is avaiable it can be set to indicate the driver is working*/
 #ifdef ON_LED_ENABLE
 //   gpio_set_value(leds[0].gpio,1);
-#endif     
+#endif
     return 0;
-    
     fail2:
       gpio_free_array(input, ARRAY_SIZE(input));
     fail1:
       gpio_free_array(leds, ARRAY_SIZE(leds));
-    
     return ret;
 }
 /***************SPI Operations************************/
@@ -257,11 +255,11 @@ static int init_Spi(void){
       printk(KERN_ALERT "spi_alloc_device() failed\n");
       return -1;
     }
-    spi_device->chip_select = SPI_BUS_CS0; 
+    spi_device->chip_select = SPI_BUS_CS0;
     /* Check whether this SPI bus.cs is already claimed */
      snprintf(buff, sizeof(buff), "%s.%u",
 	      dev_name(&spi_device->master->dev),
-	      spi_device->chip_select);     
+	      spi_device->chip_select);
      pdev = bus_find_device_by_name(spi_device->dev.bus, NULL, buff);
      if (pdev) {
 	    /* We are not going to use this spi_device, so free it */
@@ -277,25 +275,25 @@ static int init_Spi(void){
 	      "Driver [%s] already registered for %s\n",
 	      pdev->driver->name, buff);
  	      status = -1;
-	  } 
-	printk(KERN_INFO "Returning !\n");  
+	  }
+	printk(KERN_INFO "Returning !\n");
 	return status;
-     }   
-    
+     }
+
     spi_device->max_speed_hz = SPI_BUS_SPEED;
     spi_device->mode = SPI_MODE_0;
     spi_device->bits_per_word = 8;
     spi_device->irq = -1;
     spi_device->controller_state = NULL;
     spi_device->controller_data = NULL;
-    
+
     strlcpy(spi_device->modalias, "RFM12_SPI", SPI_NAME_SIZE);
-    
+
     status = spi_add_device(spi_device);
     if (status < 0) {
 	spi_dev_put(spi_device);
 	printk(KERN_ALERT "spi_add_device() failed: %d\n",status);
-    } 
+    }
     return status;
 }
 
@@ -303,21 +301,21 @@ static int init_Spi(void){
 
 uint16_t xfer(uint16_t cmd) {
     int err;
-    printk(KERN_INFO "--------------XFER called \n"); 
+    printk(KERN_INFO "--------------XFER called \n");
     spi_message_init(&msg);
 
     tx_buff[0] = cmd >> 8;
     tx_buff[1] = cmd & 0xFF;
-    
+
     transfer.tx_buf = tx_buff;
     transfer.rx_buf = rx_buff;
     transfer.len = 2;
-    
+
     spi_message_add_tail(&transfer, &msg);
     err = spi_sync(spi_device, &msg);
-    
+
     if (err != 0){
-	printk(KERN_INFO "Error in xfer function \n"); 
+	printk(KERN_INFO "Error in xfer function \n");
     }
     return  (rx_buff[0]<<8) | rx_buff[1];
 }
@@ -356,7 +354,7 @@ struct spi_transfer rfm12_make_spi_transfer(uint16_t cmd, u8* tx_buf, u8* rx_buf
     tx_buf[1] = cmd & 0xff;
     return tr;
 }
-    
+
 /***************File Operations************************/
 
 static const char *id = "Hello World";
@@ -391,56 +389,56 @@ static struct miscdevice eud_dev = {
 
 void Initialize(uint8_t nodeid, uint8_t freqBand, uint8_t groupid,
 		uint8_t txPower, uint8_t airKbps) {
-	
+
 	struct spi_transfer tr1, tr2, tr3, tr4, tr5, tr6, tr7, tr8, tr9,
 	tr10, tr11, tr12, tr13, tr14,tr15,tr16;
 	struct spi_message msg;
 	u8 tx_buf[32];
 	int err;
-    
+
 	nodeID = nodeid;
 	networkID = groupid;
 	rf12_grp= groupid;
 
-	spi_message_init(&msg);	
-	
+	spi_message_init(&msg);
+
 	tr1 = rfm12_make_spi_transfer(0x0000,tx_buf,NULL);    // initial SPI transfer added to avoid power-up problem
 	tr1.cs_change = 1;
 	spi_message_add_tail(&tr1, &msg);
-	
+
 	tr2 = rfm12_make_spi_transfer(RF_SLEEP_MODE,tx_buf+2,NULL);            // DC (disable clk pin), enable lbd
 	tr2.cs_change = 1;
-	spi_message_add_tail(&tr2, &msg);		 
-	
+	spi_message_add_tail(&tr2, &msg);
+
 	tr3 = rfm12_make_spi_transfer(RF_TXREG_WRITE,tx_buf+4,NULL);           // in case we're still in OOK mode
 	tr3.cs_change = 1;
 	spi_message_add_tail(&tr3, &msg);		  // wait until RFM12B is out of power-up reset, this takes several *seconds*
-	
+
 	tr4 = rfm12_make_spi_transfer(0x80C7 | (freqBand << 4),tx_buf+6,NULL); // EL (ena TX), EF (ena RX FIFO), 12.0pF
 	tr4.cs_change = 1;
 	spi_message_add_tail(&tr4, &msg);
-	
+
 	tr5 = rfm12_make_spi_transfer(0xA640,tx_buf+8,NULL); // Frequency is exactly 434/868/915MHz (whatever freqBand is)
 	tr5.cs_change = 1;
 	spi_message_add_tail(&tr5, &msg);
-	
+
 	tr6 = rfm12_make_spi_transfer(0xC600 + airKbps,tx_buf+10,NULL);   //Air transmission baud rate: 0x08= ~38.31Kbps
 	tr6.cs_change = 1;
 	spi_message_add_tail(&tr6, &msg);
-	
+
 	tr7 = rfm12_make_spi_transfer(0x94A2,tx_buf+12,NULL);                   // VDI,FAST,134kHz,0dBm,-91dBm
 	tr7.cs_change = 1;
 	spi_message_add_tail(&tr7, &msg);
-	
+
 	tr8 = rfm12_make_spi_transfer(0xC2AC,tx_buf+14,NULL);                   // AL,!ml,DIG,DQD4
 	tr8.cs_change = 1;
 	spi_message_add_tail(&tr8, &msg);
-	
+
 	if (networkID != 0) {
 		tr9 = rfm12_make_spi_transfer(0xCA83,tx_buf+16,NULL);               // FIFO8,2-SYNC,!ff,DR
 		tr9.cs_change = 1;
 		spi_message_add_tail(&tr9, &msg);
-		
+
 		tr10 = rfm12_make_spi_transfer(0xCE00 | networkID,tx_buf+18,NULL);   // SYNC=2DXX
 		tr10.cs_change = 1;
 		spi_message_add_tail(&tr10, &msg);
@@ -448,7 +446,7 @@ void Initialize(uint8_t nodeid, uint8_t freqBand, uint8_t groupid,
 		tr9 = rfm12_make_spi_transfer(0xCA8B,tx_buf+16,NULL);               // FIFO8,1-SYNC,!ff,DR
 		tr9.cs_change = 1;
 		spi_message_add_tail(&tr9, &msg);
-		
+
 		tr10 = rfm12_make_spi_transfer(0xCE2D,tx_buf+18,NULL);               // SYNC=2D
 		tr10.cs_change = 1;
 		spi_message_add_tail(&tr10, &msg);
@@ -457,7 +455,7 @@ void Initialize(uint8_t nodeid, uint8_t freqBand, uint8_t groupid,
 	tr11 = rfm12_make_spi_transfer(0xC483,tx_buf+20,NULL);                   // @PWR,NO RSTRIC,!st,!fi,OE,EN
 	tr11.cs_change = 1;
 	spi_message_add_tail(&tr11, &msg);
-	
+
 	tr12 = rfm12_make_spi_transfer(0x9850 | (txPower > 7 ? 7 : txPower),tx_buf+22,NULL); // !mp,90kHz,MAX OUT
 	tr12.cs_change = 1;
 	spi_message_add_tail(&tr12, &msg);
@@ -465,21 +463,21 @@ void Initialize(uint8_t nodeid, uint8_t freqBand, uint8_t groupid,
 	tr13 = rfm12_make_spi_transfer(0xCC77,tx_buf+24,NULL);                   // OB1, OB0, LPX, ddy, DDIT, BW0
 	tr13.cs_change = 1;
 	spi_message_add_tail(&tr13, &msg);
-	
+
 	tr14 = rfm12_make_spi_transfer(0xE000,tx_buf+26,NULL);                   // NOT USE
 	tr14.cs_change = 1;
 	spi_message_add_tail(&tr14, &msg);
-	
+
 	tr15 = rfm12_make_spi_transfer(0xC800,tx_buf+28,NULL);                   // NOT USE
 	tr15.cs_change = 1;
 	spi_message_add_tail(&tr15, &msg);
-	
+
 	tr16 = rfm12_make_spi_transfer(0xC049,tx_buf+30,NULL);                   // 1.66MHz,3.1V
 	/*
-	* Here no CS  Change cause it is the last message 
+	* Here no CS  Change cause it is the last message
 	*/
 	spi_message_add_tail(&tr16, &msg);
-	
+
 	err = spi_sync(spi_device, &msg);
 	if (err){
 	      printk(KERN_INFO "Error sending second SPI Message !\n");
@@ -538,7 +536,7 @@ static int __init RFM_init(void)
     /* Register GPIO and Interrupt)*/
     status = init_Gpio();
     if (status!= 0){
-      return status; 
+      return status;
     }
     /*Reguster SPI Communication*/
    status = init_Spi();
@@ -554,7 +552,7 @@ static int __init RFM_init(void)
       /* unregister */
       gpio_free_array(leds,ARRAY_SIZE(leds));
       gpio_free_array(input, ARRAY_SIZE(input));
-      return status; 
+      return status;
     }
     /*Initialize the RFM12*/
     Initialize(CLIENT_MBED_NODE, RF12_433MHZ, 212,0,0x08);
@@ -565,14 +563,14 @@ static int __init RFM_init(void)
 
 static void __exit RFM_exit(void)
 {
-    int i; 
+    int i;
     //free spi
     spi_unregister_device(spi_device);
     spi_unregister_driver(&RFM12_driver);
 
     for(i = 0; i < ARRAY_SIZE(leds); i++){
 	gpio_set_value(leds[i].gpio,0);
-    } 
+    }
     //free file structure
     misc_deregister(&eud_dev);
     // free irqs
